d3251f155f25929bca5db04cffc5f1a15d7d20cd
jagernau <stepanenkov25@gmail.com>
2022-06-27 19:26:02 +0300

    new4

--- views/movies.py
+++ views/movies.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import make_response, request
 from flask_restx import Resource, Namespace
 from implemented import movie_service
 from dao.model.movies import MovieSchema
@@ -17,22 +17,28 @@ class MoviesView(Resource):
 
     def post(self):
         new_movie = movie_service.create_movie(request.json)
-        return "", 201, {'location: f"{movie_ns.patch}/{new_movie.id}"'}
+        resp = make_response("", 201)
+        resp.headers['location'] = f"{movie_ns.path}/{new_movie.id}"
+        return resp
 
 
 
 @movie_ns.route("/<int:movie_id>")
 class MovieView(Resource):
+    schema = MovieSchema()
+
 
     def get(self, movie_id: int):       
-        return movie_service.get_movies(movie_id)
+        return self.schema.dump(movie_service.get_movies(movie_id)), 200
+
 
     def patch(self, movie_id: int):
-        return movie_service.update_movie_partial(movie_id, request.json)
+        return  self.schema.dump(movie_service.update_movie_partial(movie_id, request.json)), 200
 
 
     def put(self, movie_id):        
-        return movie_service.update_movie_full(movie_id, request.json)   
+        return self.schema.dump(movie_service.update_movie_full(movie_id, request.json)), 200
+
 
     def delete(self, movie_id):
         movie_service.delete(movie_id)
